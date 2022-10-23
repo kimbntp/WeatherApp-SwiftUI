acd228ab2ffc56fceba93b202d47704873d8b3de
kimbntp <kimb.ntp@gmail.com>
2022-10-23 15:10:25 +0700

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Natcha Taotong on 23/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
